b13b18d4e40fdf20ccbfe85cd8d3958013e1a7ac
Shijie-Xiao <109151242+Shijie-Xiao@users.noreply.github.com>
2025-04-24 04:42:08 +0000

    Deploying to gh-pages from @ Shijie-Xiao/Shijie-Xiao.github.io@15744e63ecea608846c39fec904418058950283c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "working to publish some interesting research",
+          description: "Still working and wish to publish some interesting research. To be updated.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
